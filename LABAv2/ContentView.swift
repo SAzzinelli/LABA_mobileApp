aef84c93faa9ba554181882ffafacdad036c4fe2
Simone Azzinelli <simone.azzinelli@labafirenze.coim>
2025-08-12 17:27:41 +0200

    Initial Commit

---
+++ LABAv2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LABAv2
+//
+//  Created by Simone Azzinelli on 12/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LABAv2/LABAv2App.swift
@@ -0,0 +1,17 @@
+//
+//  LABAv2App.swift
+//  LABAv2
+//
+//  Created by Simone Azzinelli on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LABAv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
